61f56a3357ad58eafccfa802b98817f5e7f72058
JakeJanssen <jakejanssen@utexas.edu>
2019-03-12 20:21:38 -0500

    Add files via upload

---
+++ introGame.py
@@ -0,0 +1,83 @@
+from textGame import *
+
+player = Player('Collin')
+
+# init terrain
+world = Terrain(8,7,player)
+
+
+gold = Item('gold', 5, 0)
+
+bundle = Item('Carefully Wrapped Package',1,1)
+
+alabaster = Npc('Alazi Alabaster', ['My daddy isn\'t here right now if that\'s what you\'re wondering.'] ,rewardDialog=['A small girl of 8 at the most approaches. \"D-do... Do you have it?? OH thank you thank you thank you Mister! I got into daddy\'s mead cabinet, and I don\'t know what would have happened if I hadn\'t gotten mom back by the time he got back from his travels.\" She unwraps the bundle, revealing a neatly stacked pile of bones.'], activateItems=[bundle],rewards= [gold])
+
+AlabasterHouse = Square([2,1], 'This is the Alabaster House.',occupants=[alabaster])
+
+dirtPatch = Square([1,2], 'This small mud puddle is not the best place to hang out. Your house is to the south.')
+
+CrowsBottomCenter = Square([2,2], 'Looking around, Crow\'s Bottom leaves something to be desired... But who knows if there\'s even anything better out there? YOU\'VE certainly never been. The Alabaster house sits, unimpressively, to the North.' )
+
+house = Square([1,3], 'You are in the small wooden box that you call home. It isn\'t much, but it keeps you dry. \nThere is a door on the east wall.')
+
+post = Note('Crows Bottom Flyer',1,0,'''
+ATTENTION! HELP!
+I had an emotional night the other night.
+It's not like me, but in a fit of feelings,
+I lost control of my better judgement...
+I buried something by the shore but I don't 
+know where. If you can return it to me you'll be
+paid handsomely.
+
+Please and thank you,
+A. Alabaster''')
+
+townPost = Square([2,3], 'You are on an overgrown patch of dry land. Your house is to the West. There is a wooden post here with a flyer attached to it.', [post])
+
+forest1 = Square([2,4], 'You are in a fairly overgrown forest. However you can make your way through the branches with some effort.')
+
+forest2 = Square([2,5], 'The many branches hanging around your head and stabbing your neck make it difficult to walk. There seems to be a bit more light coming through the trees to the south.')
+
+forest3 = Square([3,4], 'Every part of this forest looks so similar. I hope you don\'t get lost.')
+
+forest4 = Square([3,5], 'Who knows what\'s lurking behind these leaves, and slithering around your feet... I hope this is worth it.')
+
+forest5 = Square([4,4], 'Screw this. You are now against recycling. This is crazy.')
+
+shovel = Item('shovel', 1, 1)
+
+forest6 = Square([4,5], 'This is probably the prettiest part of the forest you\'ve been in.', [shovel])
+
+
+burialSpot = Object('burial spot', [shovel], [bundle])
+
+fisherman = Npc('Fisherman', ['He growls. \"Are you here like those other bastards? I\'m tryna enjoy my fishin\'. I\'ll show you where if you\'ll make it quick and scram.\" \n Burial spot learned.'])
+
+shore = Square([2,6], 'A sandy shore talstretches almost up to the treeline. A grisled man sits on a log, clutching a bent fishing rod and a small glass bottle.', occupants=[fisherman], objects=[burialSpot])
+
+grassyKnoll = Square([3,3], 'You are on a dirt path cutting through a pleasant meadow. You can make out some sort of structure to the East, and a thick treeline to the South.')
+
+guard = Npc('Guard', ['He bellows. \"What business could YOU have with the royal guard?? A shovel? You should have just said so. I think I left one laying around in the forest somewhere.\"'])
+
+guardPost = Square([4,3], 'There are two menacing, fully-armored, mean guards. I guess you don\'t KNOW they\'re mean. But... I mean, they\'re guards.', occupants=[guard])
+
+
+world.addSquare(AlabasterHouse)
+world.addSquare(dirtPatch)
+world.addSquare(CrowsBottomCenter)
+world.addSquare(house)
+world.addSquare(townPost)
+world.addSquare(forest1)
+world.addSquare(forest2)
+world.addSquare(forest3)
+world.addSquare(forest4)
+world.addSquare(forest5)
+world.addSquare(forest6)
+world.addSquare(shore)
+world.addSquare(grassyKnoll)
+world.addSquare(guardPost)
+
+game = Turn(player, world)
+
+while True:
+    game.nextTurn()
---
+++ textGame.py
@@ -0,0 +1,198 @@
+class Person:
+    def __init__(self, name, movement, wealth, inventory = []):
+        self.name = name
+        self.movement = movement
+        self.wealth = wealth
+        self.inventory = inventory
+
+class Npc(Person):
+    def __init__(self, name, dialog ,movement=1, wealth=0, gifts = [], activateItems=[], rewards = [], rewardDialog = []) :
+        Person.__init__(self, name, movement, wealth)
+        self.gifts = gifts
+        self.activateItems = activateItems
+        self.rewards = rewards
+        self.dialog = dialog 
+        self.dialogCounter = 0
+        self.rewardDialog = rewardDialog
+        self.rewardDialogCounter = 0
+
+    def speak(self):
+        print(self.name+':',self.dialog[self.dialogCounter])
+        takenItems = []
+        if len(self.gifts) > 0:
+            for item in self.gifts:
+                answer = input('Do you want ' + item.name + '?: ')
+                if answer == 'y':
+                    takenItems.append(item)
+                    self.gifts.remove(item)
+        return takenItems
+
+    def activate(self, playerItem):
+        gainedItems = []
+        if playerItem in self.activateItems:
+            print(self.rewardDialog[self.rewardDialogCounter])
+            for item in self.rewards:
+                answer = input('Do you want ' + item.name + '?: ')
+                if answer == 'y':
+                    self.rewards.remove(item)
+                    gainedItems.append(item)
+        return gainedItems 
+
+class Object:
+    def __init__(self, name, activateItems=[], heldItems=[]):
+        self.name = name
+        self.activateItems = activateItems
+        self.heldItems = heldItems 
+
+    def activate(self, playerItem):
+        gainedItems = []
+        if playerItem in self.activateItems:
+            for item in self.heldItems:
+                answer = input('Do you want ' + item.name + '?: ')
+                if answer == 'y':
+                    self.heldItems.remove(item)
+                    gainedItems.append(item)
+        return gainedItems 
+
+
+class Player(Person):
+    def __init__(self, name):
+        Person.__init__(self, name, 1, 100)
+        self.x = 1
+        self.y = 3
+        
+    def addItem(self,item):
+        self.inventory.append(item)
+                
+class Item:
+    def __init__(self, name, quantity, weight):
+        self.name = name
+        self.quantity = quantity
+        self.weight = weight
+    def use(self):
+        print('This item has no use at the moment.')
+
+class Note(Item):
+    def __init__(self, name, quantity, weight, text):
+        Item.__init__(self, name, quantity, weight)
+        self.text = text
+        
+    def use(self):
+        print(self.text)
+        
+        
+class Square():
+    def __init__(self, location, description, items=[], occupants=[], objects=[]):
+        self.location = location
+        self.items = items
+        self.description = description
+        self.occupants = occupants
+        self.objects = objects
+    
+    def removeItem(self, item):
+        self.items.remove(item)
+        
+    def addItem(self,item):
+        self.items.append(item)
+        
+    def addOccupants(self, person):
+        self.occupants.append(person)
+        
+class Terrain:
+    def __init__(self, xSize, ySize, player):
+        self.player = player 
+        self.squares = [[Square([x,y],'You get the feeling you\'ve gone too far...',[]) for y in range(ySize)] for x in range(xSize)] 
+        
+    def addSquare(self, square):
+        self.squares[square.location[0]][square.location[1]] = square 
+        
+class Turn():
+    def __init__(self, player, terrain):
+        self.player = player
+        self.terrain = terrain
+        self.square = terrain.squares[player.x][player.y]
+        self.newRoom = True
+
+    def command(self, action):
+        if action=='n':
+            self.player.y = self.player.y-1
+        elif action =='s':
+            self.player.y = self.player.y+1
+        elif action == 'e':
+            self.player.x = self.player.x+1
+        elif action == 'w':
+            self.player.x = self.player.x-1
+        elif action == 'no':
+            pass
+        elif action == 'inv':
+            for item in self.player.inventory:
+                print(item.name)
+        elif action == 'loc':
+            print(self.player.x, self.player.y)
+
+        elif 'use ' in action and ' on ' in action:
+            itemName = action[4:action.find(' on ')]
+            obLoc = action.find(' on ') + 4
+            objectName = action[obLoc:]
+            gainedItems = []
+            for item in self.player.inventory:
+                if itemName == item.name:
+                    for curObject in self.square.objects:
+                        if curObject.name == objectName:
+                            gainedItems = curObject.activate(item)
+                    for occupant in self.square.occupants:
+                        if occupant.name == objectName:
+                            gainedItems = occupant.activate(item)
+            for gain in gainedItems:
+                self.player.addItem(gain)
+
+        elif action[0:3] == 'use':
+            itemName = action[4:]
+            print(itemName)
+            for item in self.player.inventory:
+                if itemName == item.name:
+                    item.use()
+        elif action[0:4] == 'take':
+            itemName = action[5:]
+            for item in self.square.items:
+                if itemName == item.name:
+                    self.player.addItem(item)
+                    self.square.removeItem(item)
+
+        elif action[0:7] == 'talk to':
+            ncp = action[8:]
+            for occupant in self.square.occupants:
+                if occupant.name == ncp:
+                    giftedItems = occupant.speak()
+                    for item in giftedItems:
+                        self.player.addItem(item)
+        
+        else:
+            print('not a command')
+        if action in ['n','s','e','w']:
+            self.newRoom = True 
+        else:
+            self.newRoom = False
+
+        
+    def nextTurn(self):
+        if self.newRoom == True:
+            print(self.terrain.squares[self.player.x][self.player.y].description)
+            if len(self.square.items) > 0:
+                print('You find: ')
+                for item in self.square.items:
+                    print(item.name)
+
+            if len(self.square.occupants) > 0:
+                print('Occupants: ')
+                for occupant in self.square.occupants:
+                    print(occupant.name)
+
+            if len(self.square.objects) > 0:
+                print('You see: ')
+                for objects in self.square.objects:
+                    print(objects.name)
+        
+        action = input('Command: ')
+        self.command(action)
+        self.square = self.terrain.squares[self.player.x][self.player.y]
