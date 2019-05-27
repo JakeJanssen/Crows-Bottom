b90e97c6a4060b413d76111c91f9637da14edcc7
Colin Youngblood <colinyoungblood@Colins-MacBook-Pro.local>
2019-05-26 21:01:00 -0500

    fixed directions

--- testSpace.py
+++ testSpace.py
@@ -6,14 +6,15 @@ world = Terrain(5,5)
 
 rattler = Npc('Fearsome Rattler', ['hhsssss...'])
 
-topLeft = Square([0,0], 'nw')
-topMiddle = Square([0,1], 'n')
-topRight = Square([0,2], 'ne')
-middleLeft = Square([1,0], 'e')
+bottle = Weapon('glass bottle', 73)
+topLeft = Square([0,0], 'ne')
+topMiddle = Square([0,1], 'w')
+topRight = Square([0,2], 'sw')
+middleLeft = Square([1,0], 'n')
 center = Square([1,1], 'Oh fuck me... I think that\'s a rattler...', occupants=[rattler])
-middleRight = Square([1,2], 'w')
-bottomLeft = Square([2,0], 'sw')
-bottomMiddle = Square([2,1], 's')
+middleRight = Square([1,2], 'You\'ve gotta be kidding me', occupants=[rattler])
+bottomLeft = Square([2,0], 'ne')
+bottomMiddle = Square([2,1], 'e', objects=[bottle])
 bottomRight = Square([2,2], 'se')
 
 
